6414eeb7163e0373390798b44e12f2d85bd37476
miso4u <miso4u@gmail.com>
2020-09-18 02:13:06 +0900

    최종 완성 코드

---
+++ src/routes/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './About.css';
+
+function About(props) {
+  console.log(props)
+  return (
+    <div className="about__container">
+      <span>
+        미소냥 푸하하하ㅏ하
+      </span>
+      <span>
+        2020년 미소뽀유
+      </span>
+    </div>
+  )
+}
+
+export default About;
